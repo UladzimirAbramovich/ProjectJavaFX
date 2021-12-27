31dbc9280097a505d63726cef856d97abff17a0b
UladzimirAbramovich <uladzimirabramovich@gmail.com>
2021-12-28 00:37:15 +0300

    28122021-2

--- src/main/java/by/stormnet/projectjavafx/ReadThread.java
+++ src/main/java/by/stormnet/projectjavafx/ReadThread.java
@@ -53,6 +53,7 @@ public class ReadThread implements Runnable{
             System.exit(0);
         }
         System.out.printf("Поток %s завершился \n", Thread.currentThread().getName());
+
     }
 }
 
