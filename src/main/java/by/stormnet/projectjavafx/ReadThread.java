d99fa121a88a3d5204b7cffbbce7293d516f750d
UladzimirAbramovich <uladzimirabramovich@gmail.com>
2022-01-05 17:14:44 +0300

    05012022-3

--- src/main/java/by/stormnet/projectjavafx/ReadThread.java
+++ src/main/java/by/stormnet/projectjavafx/ReadThread.java
@@ -2,11 +2,9 @@ package by.stormnet.projectjavafx;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 import java.util.Iterator;
 import by.stormnet.projectjavafx.models.Record;
 import org.apache.poi.ss.usermodel.Cell;
